e14fcffb954f81d5beaa7be518a5511d32343c65
Coinduke <102346011+Coinduke@users.noreply.github.com>
2025-05-14 21:05:40 +1000

    Update car game.py
    Tweaked shader and settings in default engine to work properly on Windows, Made game less saturated and look nicer

--- car game.py
+++ car game.py
@@ -2,8 +2,143 @@
 from ursina import *
 from random import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-from numpy import round
-from ursina.shaders import lit_with_shadows_shader 
+from numpy import round 
+from ursina.shader import Shader
+from ursina.vec2 import Vec2
+
+
+lit_with_shadows_shader = Shader(language=Shader.GLSL, name='lit_with_shadows_shader', vertex = '''#version 150
+uniform struct {
+    vec4 position;
+    vec3 color;
+    vec3 attenuation;
+    vec3 spotDirection;
+    float spotCosCutoff;
+    float spotExponent;
+    sampler2DShadow shadowMap;
+    mat4 shadowViewMatrix;
+} p3d_LightSource[1];
+
+
+uniform mat4 p3d_ModelViewProjectionMatrix;
+uniform mat4 p3d_ModelViewMatrix;
+
+in vec4 vertex;
+in vec3 normal;
+in vec4 p3d_Color;
+
+in vec2 p3d_MultiTexCoord0;
+uniform vec2 texture_scale;
+uniform vec2 texture_offset;
+out vec2 texcoords;
+
+out vec4 vertex_color;
+
+uniform mat3 p3d_NormalMatrix;
+out vec3 vertex_position;
+out vec3 normal_vector;
+out vec4 shadow_coord[1];
+
+
+void main() {
+    gl_Position = p3d_ModelViewProjectionMatrix * vertex;
+    vertex_position = vec3(p3d_ModelViewMatrix * vertex);
+    normal_vector = normalize(p3d_NormalMatrix * normal);
+    shadow_coord[0] = p3d_LightSource[0].shadowViewMatrix * vec4(vertex_position, 1);
+    texcoords = (p3d_MultiTexCoord0 * texture_scale) + texture_offset;
+    vertex_color = p3d_Color;
+}
+
+''',
+fragment='''
+#version 150
+uniform struct {
+    vec4 position;
+    vec3 color;
+    vec3 attenuation;
+    vec3 spotDirection;
+    float spotCosCutoff;
+    float spotExponent;
+    sampler2DShadow shadowMap;
+    mat4 shadowViewMatrix;
+} p3d_LightSource[1];
+
+const float M_PI = 3.141592653589793;
+
+uniform sampler2D p3d_Texture0;
+uniform vec4 p3d_ColorScale;
+in vec2 texcoords;
+
+
+in vec4 vertex_color;
+out vec4 fragment_color;
+
+in vec3 vertex_position;
+in vec3 normal_vector;
+in vec4 shadow_coord[1];
+
+uniform float shadow_bias;
+uniform vec4 shadow_color;
+uniform float shadow_blur;
+uniform int shadow_samples;
+
+vec4 cast_shadows(vec4 color) {
+    float shadow_samples_float = float(shadow_samples);
+    float half_shadow_blur = shadow_blur / 2.0;
+
+    for (int i = 0; i < p3d_LightSource.length(); ++i) {
+        vec3 diff = p3d_LightSource[i].position.xyz - vertex_position * p3d_LightSource[i].position.w;
+        vec3 L = normalize(diff);
+
+        float NdotL = clamp(dot(normal_vector, L), 0.001, 1.0);
+        vec3 light_contribution = NdotL * p3d_LightSource[i].color / M_PI;
+
+        vec4 shadow_coordinates = shadow_coord[i];
+        shadow_coordinates.z += shadow_bias;
+
+        vec3 converted_shadow_color = (vec3(1.0, 1.0, 1.0) - shadow_color.rgb) * shadow_color.a;
+        color.rgb *= p3d_LightSource[i].color.rgb;
+
+        float shadow = 0.0;
+        for (int x = 0; x < shadow_samples; ++x) {
+            for (int y = 0; y < shadow_samples; ++y) {
+                float dx = float(x) * shadow_blur / shadow_samples_float - half_shadow_blur;
+                float dy = float(y) * shadow_blur / shadow_samples_float - half_shadow_blur;
+                vec4 coord = shadow_coordinates;
+                coord.x += dx;
+                coord.y += dy;
+                shadow += textureProj(p3d_LightSource[i].shadowMap, coord);
+            }
+        }
+        shadow /= (shadow_samples_float * shadow_samples_float);
+
+        color.rgb += shadow * converted_shadow_color;
+        color.rgb += light_contribution - converted_shadow_color;
+    }
+
+    return color;
+}
+
+void main() {
+    fragment_color = texture(p3d_Texture0, texcoords) * p3d_ColorScale * vertex_color;
+
+    // Call the function to handle lighting and shadowing
+    fragment_color = cast_shadows(fragment_color);
+}
+
+''',
+default_input = {
+    'texture_scale': Vec2(1,1),
+    'texture_offset': Vec2(0,0),
+    'shadow_color' : color.rgba(0, .5, 1, .25),
+    'shadow_bias': 0.001,
+    'shadow_blur': 0.001,
+    'shadow_samples': 4,
+    }
+)
+
+
+
 
 app = Ursina()  # set up the app
 
@@ -12,10 +147,7 @@ pivot = Entity()
 Entity.default_shader = lit_with_shadows_shader
 
 # Add a directional light with shadows enabled
-DirectionalLight(parent=pivot, y=2, z=3, shadows=True, rotation=(30, 0, 0), shadow_map_resolution = Vec2(2048, 2048))
-
-# Add an ambient light to simulate bounce lighting and adjust its brightness
-AmbientLight(color=color.rgba(60, 60, 60, 0.1), shadows = True)
+DirectionalLight(parent=pivot, y=2, z=3, shadows=True, rotation=(30, 0, 0), shadow_map_resolution = Vec2(4096, 4096), color=color.rgba(0.7, 0.6, 0.55, 1))
 
 
 

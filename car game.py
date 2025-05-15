bd3ecc925d5a2ae826e0bc35a232ee1ac954aeb1
Coinduke <102346011+Coinduke@users.noreply.github.com>
2025-05-15 11:27:40 +1000

    Update car game.py
    even better now

--- car game.py
+++ car game.py
@@ -1,6 +1,5 @@
 # import modules necessary for the code
 from ursina import *
-from random import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from numpy import round 
 from ursina.shader import Shader
@@ -155,32 +154,25 @@ def num_to_range(num, inMin, inMax, outMin, outMax):
   return outMin + (float(num - inMin) / float(inMax - inMin) * (outMax
                   - outMin))   # makes a function that makes the number lerp/ clamps it
 
-colliding = 0
-turn = 0
-carcam = FirstPersonController(rotation = -90)
+carcam = FirstPersonController()
 carcam.speed = 0
-carcam.mouse_sensitivity = Vec2(40,0)
+carcam.gravity = 0
+carcam.mouse_sensitivity = Vec2(40,10)
 carcam.jump_height = 0
-carcam.y = 5
+carcam.y = 50
 camera.z = -6.5
 mouse.enabled = True  # sets up variables i use in the project and sets up the camera
 
-printspeed = 0
-speed = 0
 velocity = 0
 
 model = load_model('car.obj')
-texture = load_texture('cartex.png')
-
-
-randy = 0  # more variables and loads the models and textures of the car.
+texture = load_texture('cartex.png')    # more variables and loads the models and textures of the car.
 
 sky = Sky(texture='skybox_texture.png')
 window.fullscreen = True # sets up sky and window to fullsreen
 
-car = Entity(model=model, texture=texture, scale=1)
-ground = Entity(model = 'plane', collider = 'mesh', scale = 600, texture = 'track.png')  # makes the car and ground entities
-
+car = Entity(model=model,texture=texture, scale=1)
+ground = Entity(model = 'plane', scale = 600, texture = 'track.png')  # makes the car and ground entities
 turnrate = 90 # sets the turnrate to 90 because wheel model is rotated the wrong way by default
 
 wheel1 = Entity(model='wheel.obj', texture='wheeltex.png', scale = 0.31)
@@ -189,73 +181,64 @@ wheel3 = Entity(model='wheel.obj', texture='wheeltex.png', scale = 0.31)
 wheel4 = Entity(model='wheel.obj', texture='wheeltex.png', scale = 0.31)   # makes all the wheel entities
 
 def update():
-    global velocity, speed, model, texture, randy,turnrate, colliding, turn   # main game function(inbuilt to ursina engine)
+    global velocity, speed, model, texture,turnrate  # main game function(inbuilt to ursina engine)
 
-    carcam.position += carcam.forward * velocity/2
-    car.position = carcam.position
-    car.rotation = carcam.rotation 
-    car.y = carcam.y + 0.0625 + randy
-    car.x = carcam.x + 1.5
-    car.z = carcam.z + 0.5
-    suspension_offset = uniform(0.0,0.01) * velocity             # get camera to follow car and rotate with mouse or keyboard inputs
-
-    if car.intersects(ground).hit:  
-        colliding= 1
-    else: colliding = 0   # checks if car is touching ground and sets a variable depending on that.
-
-    printspeed = str(round(velocity * 9.5 * 1.609))   # sets the speed based on car's velocity
-
-    print_on_screen(text = printspeed, duration = 0.001, position = (0, 0.45)) # puts text on screem
-
+    car.position += car.forward * velocity/2
+    carcam.position = car.position
+    carcam.rotation_x = car.rotation_x
+    carcam.rotation_y = car.rotation_y
 
     if velocity <= -3.49129086168:  # sets a speed limit if car is in reverse
         velocity = -3.49129086168
 
-    if carcam.y < -0.1 or held_keys['r']:     # checks if r is pressed and resets car position if it is
-        carcam.position = (0,0,0)
+    if car.y < -0.1 or held_keys['r']:     # checks if r is pressed and resets car position if it is
+        car.position = (0,0,0)
         velocity = 0
-        carcam.rotation = 0
+        car.rotation = 0
 
     if held_keys['w']:
 
         if not held_keys['s']:
-            if not car.y >= 1:
+            if not car.y >= 1:          #fix collision detection later
                 velocity += 0.03
-                velocity *= 0.997
+                velocity *= 0.993
 
-        else: velocity = velocity * 0.97
+        else: velocity *= 0.97 - 0.01
 
     else:
         if velocity > 0:
-            velocity = velocity - 0.001     # check if w is pressed and the brake isn't and adds velocity to the car, else it will make the car gradually slow down
-    
+            velocity = (velocity * 0.997) -0.001   # check if w is pressed and the brake isn't and adds velocity to the car, else it will make the car gradually slow down
+
     if held_keys['s']:
 
         if not held_keys['w']:
             if not car.y >= 1:
-                velocity -= 0.01
-                velocity *= 0.997
-                camera.z = 6.5-velocity
-                camera.rotation_y = 180  # checks if brake key is pressed, if it is car will slow, if w is pressed it won't slow, puts camera in reverse mode
+                if velocity <= 0:
+                    velocity -= 0.01
+                    velocity *= 0.997
+                    camera.z = 6.5-velocity
+                    camera.rotation_y = 180
+                else:
+                    velocity *= 0.97 - 0.01
 
-        else: velocity = velocity * 0.97
+        else: velocity *= 0.97 - 0.01
 
     else:
          if velocity < 0:
-            velocity = velocity + 0.001
+            velocity = (velocity * 0.997)  +0.001
 
     if held_keys['a']:
-        carcam.rotation_y -= (4 - velocity/2) * num_to_range(velocity, 0, 4, 0, 1)
-        velocity /= 1.003
+        car.rotation_y -= (4 - velocity/2) * num_to_range(velocity, 0, 4, 0, 1)
+        velocity *= 0.995
         turnrate = 60
     elif held_keys['d']:
         turnrate = 120
     else: turnrate = 90
 
     if held_keys['d']:
-        carcam.rotation_y += (4 - velocity/2) * num_to_range(velocity, 0, 4, 0, 1)
+        car.rotation_y += (4 - velocity/2) * num_to_range(velocity, 0, 4, 0, 1)
         turnrate = 110
-        velocity /= 1.003
+        velocity *= 0.995
         
     elif held_keys['a']:
         turnrate = 70
@@ -270,7 +253,7 @@ def update():
 
     wheel1.position += car.right * 0.65
     wheel1.position += car.back * 1
-    wheel1.y = 0.32 + suspension_offset
+    wheel1.y = 0.32
 
     wheel1.rotation_x = car.rotation_x  
     wheel1.rotation_y = car.rotation_y + 90
@@ -280,7 +263,7 @@ def update():
 
     wheel2.position += car.left * 0.65
     wheel2.position += car.back * 1
-    wheel2.y = 0.32 + suspension_offset
+    wheel2.y = 0.32
 
     wheel2.rotation_x = car.rotation_x  
     wheel2.rotation_y = car.rotation_y + 90
@@ -290,7 +273,7 @@ def update():
 
     wheel3.position += car.left * 0.65
     wheel3.position += car.forward * 1.45
-    wheel3.y = 0.32 + suspension_offset
+    wheel3.y = 0.32
 
     wheel3.rotation_x = car.rotation_x 
     wheel3.rotation_y = car.rotation_y + turnrate
@@ -300,17 +283,16 @@ def update():
 
     wheel4.position += car.right * 0.65
     wheel4.position += car.forward * 1.45
-    wheel4.y = 0.32 + suspension_offset
+    wheel4.y = 0.32
 
     wheel4.rotation_x = car.rotation_x 
     wheel4.rotation_y = car.rotation_y + turnrate
 
     if velocity < 0:
-        camera.z = 6.5-velocity
+        camera.z = 6.5
         camera.rotation_y = 180
     else:
         camera.z = -6.5
         camera.rotation_y = 0
-
 
 app.run()
